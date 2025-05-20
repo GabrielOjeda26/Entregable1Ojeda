fcc9d19039c3d9b19551c292507992cbd012e943
Gabriel Ojeda <gabrielojeda2605@gmail.com>
2025-05-20 19:13:23 -0300

    Entregable2Ojeda

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Trabajo Practico N°1 Fernando Gabriel Ojeda
 
 
-const buscarNombreProducto = document.getElementById("buscarNombreProducto");
-const buscarCodigo = document.getElementById("buscarCodigo");
-const cargarProducto = document.getElementById("cargarProducto");
-const cambiarPrecio = document.getElementById("cambiarPrecio");
-const eliminarProducto = document.getElementById("eliminarProducto");
-const resultado = document.getElementById("resultado");
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+if (usuarios.length === 0) {
+  const administrador = {
+    nombre: "Gabriel",
+    apellido: "Ojeda",
+    fechaNacimiento: "26/05/2000",
+    dni: "42712656",
+    esAdministrador: true,
+    usuario: "Gabi",
+    contraseña: "1234"
+  };
+  usuarios.push(administrador);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+}
 
 
 const productos = [
@@ -228,8 +236,195 @@ function eliminar() {
 }
 
 
-buscarPorNombre();
-buscarPorCodigo();
-nuevoProducto();
-cambioDePrecios();
-eliminar();
+function verificarUsuario() {
+  const usuarioLogueado = localStorage.getItem("usuarioLogueado");
+  if (usuarioLogueado) {
+    const usuario = JSON.parse(usuarioLogueado);
+    if (usuario.esAdministrador) {
+      mostrarContenidoAdministrador();
+    } else {
+      mostrarContenidoEmpleado();
+    }
+  } else {
+    mostrarFormularioLogin();
+  }
+}
+
+
+function mostrarFormularioLogin() {
+  const formulario = `
+    <div class="logo">
+      <span class="titulo">Minimercado</span>
+      <img class="img" src="./img/carrito-de-compras.png" alt="">
+    </div>
+    <form id="formulario-login">
+      <div class="sesion">
+        <label for="usuario" class="label">Usuario</label> Gabi(para ingresar como admin),coder(para ingresar como empleado)
+        <input type="text" id="usuario" name="usuario" class="input">1234(para ingresar como admin y como empleado)
+        <label for="contraseña" class="label">Contraseña</label>
+        <input type="password" id="contraseña" name="contraseña" class="input">
+        <div class="boton-actualizar">
+          <button type="submit">Iniciar sesión</button>
+        </div>
+      </div>
+    </form>
+    <div class="crear-usuario">
+      <button id="crear-usuario">Crear usuario</button>
+    </div>
+  `;
+  document.body.innerHTML = formulario;
+  document.getElementById("crear-usuario").addEventListener("click", mostrarFormularioCrearUsuario);
+  document.getElementById("formulario-login").addEventListener("submit", verificarCredenciales);
+}
+
+
+function mostrarFormularioCrearUsuario() {
+  const formulario = `
+    <form id="formulario-crear-usuario" class="form">
+      <div class="sesion">
+        <label for="nombre" class="label">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" class="input"><br><br>
+        <label for="apellido" class="label">Apellido:</label>
+        <input type="text" id="apellido" name="apellido" class="input"><br><br>
+        <label for="fecha-nacimiento" class="label">Fecha de nacimiento:</label>
+        <input type="date" id="fecha-nacimiento" name="fecha-nacimiento" class="input"><br><br>
+        <label for="dni" class="label">DNI:</label>
+        <input type="number" id="dni" name="dni" class="input"><br><br>
+        <label for="usuario" class="label">Usuario:</label>
+        <input type="text" id="usuario" name="usuario" class="input"><br><br>
+        <label for="contraseña" class="label">Contraseña:</label>
+        <input type="password" id="contraseña" name="contraseña" class="input"><br><br>
+        <button type="submit" class="crear-usuario">Crear usuario</button>
+        </div>
+    </form>
+  `;
+  document.body.innerHTML = formulario;
+  document.getElementById("formulario-crear-usuario").addEventListener("submit", crearUsuario);
+}
+
+
+function crearUsuario(event) {
+  event.preventDefault();
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const fechaNacimiento = document.getElementById("fecha-nacimiento").value;
+  const dni = document.getElementById("dni").value;
+  const usuario = document.getElementById("usuario").value;
+  const contraseña = document.getElementById("contraseña").value;
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  usuarios.push({
+    nombre,
+    apellido,
+    fechaNacimiento,
+    dni,
+    esAdministrador,
+    usuario,
+    contraseña
+  });
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarFormularioLogin();
+}
+
+
+function eliminarUsuarioFuncion() {
+  eliminarUsuario.addEventListener('click', () => {
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  const usuarioAEliminar = prompt("Ingrese el usuario a eliminar");
+  const usuariosActualizados = usuarios.filter(u => u.usuario !== usuarioAEliminar);
+  localStorage.setItem("usuarios", JSON.stringify(usuariosActualizados));
+  })
+}
+
+
+function verificarCredenciales(event) {
+  event.preventDefault();
+  const usuario = document.getElementById("usuario").value;
+  const contraseña = document.getElementById("contraseña").value;
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  const usuarioEncontrado = usuarios.find(u => u.usuario === usuario && u.contraseña === contraseña);
+  if (usuarioEncontrado) {
+    localStorage.setItem("usuarioLogueado", JSON.stringify(usuarioEncontrado));
+    verificarUsuario();
+  } else {
+    alert("Usuario o contraseña incorrecta");
+  }
+}
+
+
+function mostrarContenidoAdministrador() {
+  document.body.innerHTML = `
+    <div class="logo">
+            <span class="titulo">Minimercado</span>
+            <img class="img" src="./img/carrito-de-compras.png" alt="">
+    </div>  
+    <div class="botones">
+        <button id="buscarNombreProducto" class="button is-success">Buscar por nombre</button>
+        <button id="buscarCodigo" class="button is-success">Buscar por codigo</button>
+        <button id="cargarProducto" class="button is-success">Cargar nuevo producto</button>
+        <button id="cambiarPrecio" class="button is-success">Cambiar precios</button>
+        <button id="eliminarProducto" class="button is-danger">Eliminar Producto</button>
+        <button id="eliminarUsuario" class="button is-danger">Eliminar Usuario</button>
+        <button id="cerrar" class="boton-cancelar">Cerrar sesion</button>
+    </div>
+    <p id="resultado"></p>
+    <br><br>
+    <p>
+       Este simulador esta basado en un minimercado, y su funcionalidad consiste en facilitarle <br>
+       el trabajo al dueño de este minimercado, ya sea para buscar, cargar, cambiar el precio o eliminar un producto.
+    </p>
+  `;
+  const buscarNombreProducto = document.getElementById("buscarNombreProducto");
+  const buscarCodigo = document.getElementById("buscarCodigo");
+  const cargarProducto = document.getElementById("cargarProducto");
+  const cambiarPrecio = document.getElementById("cambiarPrecio");
+  const eliminarProducto = document.getElementById("eliminarProducto");
+  const eliminarUsuario = document.getElementById("eliminarUsuario");
+  const cerrar = document.getElementById("cerrar");
+  const resultado = document.getElementById("resultado");
+  buscarPorNombre();
+  buscarPorCodigo();
+  nuevoProducto();
+  cambioDePrecios();
+  eliminar();
+  eliminarUsuarioFuncion();
+  cerrarSesion();
+}
+
+
+function mostrarContenidoEmpleado() {
+  document.body.innerHTML = `
+    <div class="logo">
+      <span class="titulo">Minimercado</span>
+      <img class="img" src="./img/carrito-de-compras.png" alt="">
+    </div>  
+    <div class="botones">
+      <button id="buscarNombreProducto" class="button is-success">Buscar por nombre</button>
+      <button id="buscarCodigo" class="button is-success">Buscar por codigo</button>
+      <button id="cerrar" class="boton-cancelar">Cerrar sesion</button>
+    </div>
+    <p id="resultado"></p>
+    <br><br>
+    <p>
+       Este simulador esta basado en un minimercado, y su funcionalidad consiste en facilitarle <br>
+       el trabajo al dueño de este minimercado, ya sea para buscar, cargar, cambiar el precio o eliminar un producto.
+    </p>
+  `;
+  const buscarNombreProducto = document.getElementById("buscarNombreProducto");
+  const buscarCodigo = document.getElementById("buscarCodigo");
+  const cerrar = document.getElementById("cerrar");
+  const resultado = document.getElementById("resultado");
+  buscarPorNombre();
+  buscarPorCodigo();
+  cerrarSesion();
+}
+
+
+function cerrarSesion(){
+  cerrar.addEventListener('click', () => {
+localStorage.removeItem("usuarioLogueado");
+verificarUsuario();
+  })
+}
+
+
+verificarUsuario();
